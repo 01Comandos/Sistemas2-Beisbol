e75616818cf3d65be0e2e9a6f554e00996a75599
Comandos <00comandos@gmail.com>
2016-07-16 16:38:20 -0400

    Agregado media queries hasta 1200px y efectos hover mejorado

--- index.php
+++ index.php
@@ -219,11 +219,17 @@
 			</ul>
 
 			<!--SEPARADOR NOTIFICACIONES-->
-			<div class="u-general-actions-titleVendedor">
+			<div class="u-general-actions-titleVendedor notificaciones">
 				<figure>
 					<span class="icon-Beisbol_home"></span>
 				</figure>
 				<p>Notificaciones</p>
+				<!--BUSCAR-->
+				<div class="search">
+					<a href="#">
+						<span class="icon-Beisbol_search"></span>
+					</a>
+				</div>
 			</div>
 
 			<!--LISTADO DE BOLETOS-->
